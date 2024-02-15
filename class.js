4196ecf4b1e2df3388339281d43c56d66c0de936
LucasGabriel77 <lucasgrs01616@gmail.com>
2024-02-15 12:24:32 -0300

    detecção de teclas

--- class.js
+++ class.js
@@ -19,4 +19,42 @@ class obj{
         // des.closePath() // fim do obj
         des.stroke()    
     }
+    des_car(){
+        //trapezio  do carro
+        des.beginPath()
+        des.moveTo(this.x,this.y)
+        des.lineTo(this.x+50, this.y)
+        des.lineTo(this.x+40, this.y-50)
+        des.lineTo(this.x+10, this.y-50)
+        des.closePath()
+        des.lineWidth = '4'
+        des.strokeStyle = 'black'
+        des.fillStyle = this.a
+        des.stroke()
+        des.fill()
+    
+        
+        des.beginPath()
+        des.lineWidth = '5'
+        des.strokeStyle = 'blue'
+        des.fillStyle = this.a
+        des.rect(this.x+10,this.y-70,30,20)
+        des.closePath()
+        des.stroke()
+        des.fill()}
+    
+    // des_car(){
+    //     des.beginPath()
+    //     des.moveTo(this.x,this.y)
+    //     des.lineTo(this.x+150, this.y)
+    //     des.lineTo(this.x+150,this.y+150)
+    //     des.lineTo(this.x,this.y+150)
+    //     des.lineTo(this.x-10,this.y-10)
+    //     des.closePath()
+    //     des.lineWidth = '4'
+    //     des.strokeStyle = 'black'
+    //     des.fillStyle = this.a
+    //     des.stroke()
+    //     des.fill()
+    // }
 }
--- main.js
+++ main.js
@@ -2,10 +2,29 @@ let des = document.getElementById('des').getContext("2d")
 
 console.log(des)
 
-const quadrado1 = new obj(10,10,100,100,'orange')
-const quadrado2 = new obj(200,200,150,150, 'blue')
-const quadrado3 = new obj(300,300,100,150, 'blue')
+// const quadrado1 = new obj(10,10,100,100,'orange')
+// const quadrado2 = new obj(200,200,150,150, 'blue')
+// const quadrado3 = new obj(300,300,150,150, 'blue')
 
-quadrado1.des_obj()
-quadrado2.des_quad()
-quadrado3.des_quad()
+// quadrado1.des_obj()
+// quadrado2.des_quad()
+// quadrado3.des_quad()
+
+const carro = new obj(400,400,100,100, 'blue')
+carro.des_car()
+
+document.addEventListener('keydown', (e)=>{
+    if(e.key === 'a'){console.log(e)}
+    else if(e.key === 'w'){console.log(e)}
+    else if(e.key === 's'){console.log(e)}
+    else if(e.key === 'd'){console.log(e)}
+
+    
+    }
+
+)
+
+document.addEventListener('keyup', (e)=>{
+    // console.log(e)
+}
+)
